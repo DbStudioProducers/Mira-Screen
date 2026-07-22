b5467c3a3e0cb67cd55c06e29cd773e9654ebab0
DbStudioProducers <yggdrasilagencia@gmail.com>
2026-07-22 13:53:15 -0300

    Delete mira-screen directory

--- mira-screen/backend/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { ContentModule } from './content/content.module';
-import { GraphqlModule } from './graphql.module';
-
-@Module({
-  imports: [AuthModule, ContentModule, GraphqlModule],
-})
-export class AppModule {}
